5734df6e47c62564cbccb663a230b86242135197
Bakhyt Sembayev <bahytsembaev@gmail.com>
2024-05-07 09:51:04 +0500

    use-params

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Outlet, useParams } from 'react-router-dom';
 import styles from './App.css';
 
 const fetchProductList =()=>[
@@ -6,6 +6,14 @@ const fetchProductList =()=>[
   {id: 2, name: 'смартфон'},
   {id: 3, name: 'планшет'},
 ]
+
+const fetchProduct =(id)=>({
+  1: {id: 1, name: 'ноутбук',price:99900,amount:6},
+  2: {id: 2, name: 'смартфон',price:19900,amount:15},
+  3: {id: 3, name: 'планшет',price:30000,amount:9},
+})[id];
+  
+
 const MainPage = () => <div>Контент главной страницы</div>;
 const Catalog = () => (
 <div> 
@@ -14,14 +22,23 @@ const Catalog = () => (
   
     {fetchProductList().map(({id,name})=>(
       <li key ={id}>
-<Link to ="product"> {name} </Link>
+<Link to ={`product/${id}`}> {name} </Link>
       </li>
     ))}
   </ul> 
   <Outlet/>
 </div>
 );
-const Product = ()=> <div>Контент страницы товара</div>;
+const Product = ()=> {
+  const params = useParams ();
+  const {name, price, amount} = fetchProduct(params.id);
+return (
+<div>
+  <h3>Товар - {name} </h3>
+  <div>Цена: {price}</div>
+  <div>Количество: {amount}</div>
+</div>
+)};
 const Contacts = () => <div>Контент контактов</div>;
 
 const App = () => {
@@ -45,7 +62,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/catalog" element={<Catalog />} >
-          <Route path="product" element={<Product />} />
+          <Route path="product/:id" element={<Product />} />
             </Route>
           <Route path="/contacts" element={<Contacts />} />
         </Routes>
